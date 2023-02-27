777934fd7bad629f7e8a9b0ac4b90274228b87a1
mitsuonitta <infinias320@gmail.com>
2023-02-27 15:16:57 +0900

    Revert "Merge pull request #8 from mitsuonitta/first_training"
    This reverts commit 4764e6bb1e459b8f441296b159c0a38efa7bd6df, reversing
    changes made to 052e275861f0d21d4e6d2e86d313a19802fb1d13.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,13 +2,13 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getweek
+    getWeek
     @plan = Plan.new
   end
 
   # 予定の保存
   def create
-    plan.create(plan_params)
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
@@ -19,23 +19,24 @@ class CalendarsController < ApplicationController
   end
 
   def getWeek
-    days = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
-    @today_date = Date.today
+    @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
 
-    plans = Plan.where(date: @today_date..@today_date + 6)
+    plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month : (@today_date + x).month, :date : (@today_date + x).day, :plans : today_plans}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
       @week_days.push(days)
     end
+
   end
 end
